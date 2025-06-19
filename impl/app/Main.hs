af5c6bc5fb183ef5d342b064640ce01f4365b6e0
Marvin Borner <git@marvinborner.de>
2025-06-19 03:37:50 +0200

    Add CLI flags

--- impl/app/Main.hs
+++ impl/app/Main.hs
@@ -1,4 +1,4 @@
--- MIT License, Copyright (c) 2024 Marvin Borner
+-- MIT License, Copyright (c) 2025 Marvin Borner
 
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -6,63 +6,67 @@ module Main
   ( main
   ) where
 
+import qualified Data.Direct                   as Direct
 import qualified Data.Front                    as Front
                                                 ( Term(..) )
 import qualified Data.Lambda                   as Lambda
 import qualified Data.Text                     as T
-import qualified Data.Direct             as Direct
+import           Data.View
 import           Debug.Trace
+import           GraphRewriting.Graph
+import           GraphRewriting.Graph.Types
+import qualified Language.Direct.Reducer       as Direct
 import qualified Language.Front.Parser         as Front
-                                                ( parseProgram )
 import qualified Language.Front.Transformer.Lambda
                                                as Front
-                                                ( transformLambda )
-import qualified Language.Lambda.Transformer.Monad
-                                               as Lambda
-                                                ( transformMonad )
 import qualified Language.Lambda.Transformer.Direct
                                                as Lambda
-                                                ( transformDirect )
+import qualified Language.Lambda.Transformer.Monad
+                                               as Lambda
 import qualified Language.Monad.Reducer        as Monad
-                                                ( bench
-                                                , visualize
-                                                )
-import qualified Language.Direct.Reducer as Direct
-                                                ( bench
-                                                , visualize
-                                                )
-import           Options.Applicative            ( (<**>)
-                                                , Parser
-                                                , execParser
-                                                , fullDesc
-                                                , header
-                                                , helper
-                                                , info
-                                                )
+import           Options.Applicative
 
-data ArgMode = ArgEval
+data ArgMode = Visualize | Benchmark
 
-newtype Args = Args
-  { _argMode :: ArgMode
+data Args = Args
+  { _argMode   :: ArgMode
+  , _argTarget :: String
   }
 
-args :: Parser Args
-args = pure $ Args ArgEval
+mode :: Parser ArgMode
+mode =
+  flag' Visualize (long "visualize" <> short 'v' <> help "Visualize reduction")
+    <|> flag' Benchmark
+              (long "benchmark" <> short 'b' <> help "Benchmark reduction")
 
--- pipeline :: T.Text -> Either String Direct.Term
-pipeline input = do
+args :: Parser Args
+args = Args <$> (mode <|> pure Visualize) <*> strOption
+  (long "target" <> short 't' <> value "target" <> help
+    "target backend (direct, monad)"
+  )
+
+lambdaPipeline target input = do
   front  <- Front.parseProgram input
   lambda <- Front.transformLambda front
-  trace (show front <> "\n\n\n" <> show (Lambda.unwrap lambda))
-        (Lambda.transformMonad lambda)
+  trace (show front <> "\n\n\n" <> show (Lambda.unwrap lambda)) (pure lambda)
+
+monadPipeline Visualize =
+  either putStrLn Monad.visualize . Lambda.transformMonad
+monadPipeline Benchmark = either putStrLn Monad.bench . Lambda.transformMonad
+
+directPipeline Visualize =
+  either putStrLn Direct.visualize . Lambda.transformDirect
+directPipeline Benchmark =
+  either putStrLn Direct.bench . Lambda.transformDirect
 
 actions :: Args -> IO ()
-actions Args { _argMode = ArgEval } = do
+actions Args { _argMode = mode, _argTarget = target } = do
   program <- getContents
-  case pipeline $ T.pack program of
-    Left  err  -> putStrLn err
-    Right core -> Monad.visualize core
-      -- Monad.visualize core
+  case lambdaPipeline target $ T.pack program of
+    Left err -> putStrLn err
+    Right lambda | target == "monad" -> monadPipeline mode lambda
+                 | target == "direct" -> directPipeline mode lambda
+                 | otherwise -> putStrLn "target must be monad or direct"
 
 main :: IO ()
 main = execParser opts >>= actions
